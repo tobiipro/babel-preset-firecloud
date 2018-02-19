62ec7c7c43db8ac281e421162cdbea6b3fda9492
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2018-02-19 17:31:10 +0100

    Added debug.js and launch.json to be able quickly debug preset

---
+++ debug.js
@@ -0,0 +1,14 @@
+//
+// This file is launched with "Debug" launch option
+// @ToDo: replace with proper tests
+
+var preset = require('./index');
+
+let context = {
+};
+
+let options = {
+  'debug': true
+};
+
+preset(context, options);
