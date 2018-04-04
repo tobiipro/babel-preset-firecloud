d45062d0d52616d097f612a9b3840bcca3645a58
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2018-04-05 01:07:29 +0200

    Proper implementation of export-all

--- plugins/babel-plugin-firecloud-export-all.js
+++ plugins/babel-plugin-firecloud-export-all.js
@@ -1,66 +1,61 @@
-/* eslint-disable no-null/no-null*/
-
 // Transforms all top-level variables to be exported + appends "exports." to all references of them
-
 let _ = require('lodash');
 
 module.exports = function() {
   // eslint-disable-next-line fp/no-arguments
   let t = arguments[0].types;
 
-  // eslint-disable-next-line lodash/prefer-noop
-  let log = function() {
-    // console.log.apply(console.log, arguments)
+  let _ensureDeclarationExported = function(binding) {
+    let declarationPath;
+
+    if (t.isVariableDeclarator(binding.path.node)) {
+      declarationPath = binding.path.parentPath;
+      t.assertVariableDeclaration(declarationPath);
+    }
+
+    if (t.isFunctionDeclaration(binding.path.node)) {
+      declarationPath = binding.path;
+    }
+
+    if (declarationPath && !t.isExportNamedDeclaration(declarationPath.parent)) {
+      declarationPath.replaceWith(t.exportNamedDeclaration(declarationPath.node, [], undefined));
+    }
   };
 
-  // eslint-disable-next-line lodash/prefer-noop
-  let logVerbose = function() {
-    // console.log.apply(console.log, arguments)
-  };
+  let _ensureAccessedWithExportsDot = function(binding) {
+    // analyzing existing references to go through 'exports.'
+    // those, which already do, are not found as references by AST
+    let {referencePaths} = binding;
 
-  let exportAllVisitor = {
-
-    VariableDeclaration(path) {
-      // only top-level scope declarations
-      if (path.scope.parent !== null) {
-        path.stop();
+    _.forEach(referencePaths, function(rp) {
+      if (!t.isIdentifier(rp.node)) {
         return;
       }
 
-      let identifierNode = path.node.declarations[0].id;
-      let {name} = identifierNode;
+      let isPartOfDeclaration = rp.findParent(function(path) {
+        return path === binding.path;
+      });
 
-      // making sure that declaration has 'export' keyword
-      if (path.parent.type !== 'ExportNamedDeclaration') {
-        log(`Exporting '${name}'`);
-        logVerbose(JSON.stringify(path.node));
-        path.replaceWith(t.exportNamedDeclaration(path.node, [], null));
+      if (isPartOfDeclaration) {
+        return;
       }
 
-      // analyzing existing references to go through 'exports.'
-      // those, which already do, are not found as references by AST
-      let binding = path.scope.bindings[name];
-      let {referencePaths} = binding;
-
-      _.forEach(referencePaths, function(rp) {
-        // includes declaration reference too, not interested
-        if (!t.isIdentifier(rp.node)) {
-          return;
-        }
-
-        // all the references have to go through 'exports.'
-        log(`Replacing reference with 'exports.' for ${name}`);
-        rp.replaceWith(t.memberExpression(t.identifier('exports'), t.identifier(name)));
-      });
-    }
-
+      let me = t.memberExpression(t.identifier('exports'), t.identifier(binding.identifier.name));
+      rp.replaceWith(me);
+    });
   };
 
   return {
+    name: 'firecloud-export-all',
     visitor: {
       Program: function(path, _state) {
-        log(`Processing file: ${this.file.opts.filename}`);
-        path.traverse(exportAllVisitor);
+        let topVariables = _.filter(path.scope.bindings, function(binding) {
+          return t.isVariableDeclarator(binding.path.node) ||
+                t.isFunctionDeclaration(binding.path.node);
+        });
+
+        _.forEach(topVariables, _ensureDeclarationExported);
+        _.forEach(topVariables, _ensureAccessedWithExportsDot);
       }
     }
   };
